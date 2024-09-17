1d4c6934bf1073b31cf5f1ffda539a1838082810
Edson <edsonsoliveirajunior@gmail.com>
2024-09-17 15:33:51 -0300

    Create RelôgioJava.java
    código criado da aula04

---
+++ RelôgioJava.java
@@ -0,0 +1,9 @@
+import java.util.Date;
+public class Main
+{
+	public static void main(String[] args) {
+		Date horadosistema = new Date(); 
+		System.out.print("a hora do sistema é: ");
+		System.out.println(horadosistema.toString());
+	}
+}
